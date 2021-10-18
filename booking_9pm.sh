c24b722d1bb7f7697e6f7bd507a4eadd042c710e
anilk <anilk@orienlabs.io>
2021-10-18 18:08:50 -0400

    including locally build jar for quick execution

--- booking_9pm.sh
+++ booking_9pm.sh
@@ -1,1 +1,1 @@
-java "-DloginUser=Yourcnu@gmail.com" "-DloginPwd=game123" "-DcourtName=Badminton Courts 5-8" "-Dtimeslot=21:00" -DmaxRetryDuration=30 -jar /Users/anilk/Desktop/workspace/badminton_court_booking/build/libs/badminton_court_booking-0.0.1-SNAPSHOT-all.jar
+java "-DloginUser=Yourcnu@gmail.com" "-DloginPwd=game123" "-DcourtName=Badminton Courts 5-8" "-Dtimeslot=21:00" -DmaxRetryDuration=30 -jar releases/badminton_court_booking-0.0.1-SNAPSHOT-all.jar
